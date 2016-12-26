6547cfda4d450f416107592232253b1df0453940
Cash Foley <cashfoley@hotmail.com>
2016-12-25 23:38:54 -0600

    First pass ARM deploy

--- SqlServerPatcher/Azure/Deploy-AzureResourceGroup.ps1
+++ SqlServerPatcher/Azure/Deploy-AzureResourceGroup.ps1
@@ -1,64 +1,15 @@
-<#
- .SYNOPSIS
-    Deploys a template to Azure
-
- .DESCRIPTION
-    Deploys an Azure Resource Manager template
-
- .PARAMETER subscriptionId
-    The subscription id where the template will be deployed.
-
- .PARAMETER resourceGroupName
-    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
-
- .PARAMETER resourceGroupLocation
-    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
-
- .PARAMETER deploymentName
-    The deployment name.
-
- .PARAMETER templateFilePath
-    Optional, path to the template file. Defaults to template.json.
-
- .PARAMETER parametersFilePath
-    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
-#>
-
 param(
- [Parameter(Mandatory=$True)]
- [string]
- $subscriptionId,
-
- [Parameter(Mandatory=$True)]
- [string]
- $resourceGroupName,
-
- [string]
- $resourceGroupLocation,
-
- [Parameter(Mandatory=$True)]
- [string]
- $deploymentName,
-
- [string]
- $templateFilePath = "template.json",
-
- [string]
- $parametersFilePath = "parameters.json"
+   [string] $subscriptionName = 'Visual Studio Ultimate with MSDN'
+ , [string] $resourceGroupName = 'dbversiondebug'
+ , [string] $resourceGroupLocation = 'Central US'
+ , [string] $deploymentName = "debugDeployment"
+ , [string] $templateFilePath = "AdventureWorksDB.json"
+ , [string] $parametersFilePath = "AdventureWorksDB.parms.json"
+ , [string] $dbServerName = "debug-dbserver01"
+ , [string] $dbDatabaseName = 'db-model01'
+ , [string] $DatabaseServerAdminLogin = 'db-dev-admin01'
+ , [string] $DatabaseServerAdminLoginPassword = '5upp3r53(re7'
 )
-
-<#
-.SYNOPSIS
-    Registers RPs
-#>
-Function RegisterRP {
-    Param(
-        [string]$ResourceProviderNamespace
-    )
-
-    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
-    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
-}
 
 #******************************************************************************
 # Script body
@@ -66,42 +17,111 @@ Function RegisterRP {
 #******************************************************************************
 $ErrorActionPreference = "Stop"
 
-# sign in
-Write-Host "Logging in...";
-Login-AzureRmAccount;
+try 
+{
+    Write-Verbose 'Checking if logged into Azure'
+    $isLoggedIn = Get-AzureRmContext -ErrorAction Stop
+}
+catch [System.Management.Automation.PSInvalidOperationException] 
+{
+    Write-Verbose 'Not logged into Azure. Login now.'
+    $isLoggedIn = Add-AzureRmAccount
+}
+catch 
+{
+    Write-Error $Error[0].Exception
+}
 
 # select subscription
-Write-Host "Selecting subscription '$subscriptionId'";
-Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+Write-Host "Selecting subscription '$subscriptionName'"
+Set-AzureRmContext -SubscriptionName $subscriptionName
 
+#******************************************************************************
 # Register RPs
-$resourceProviders = @("microsoft.keyvault","microsoft.sql");
-if($resourceProviders.length) {
+#******************************************************************************
+$resourceProviders = @("microsoft.keyvault","microsoft.sql")
+
+if($resourceProviders.length) 
+{
     Write-Host "Registering resource providers"
-    foreach($resourceProvider in $resourceProviders) {
-        RegisterRP($resourceProvider);
+    foreach($resourceProvider in $resourceProviders) 
+    {
+        Write-Host "Registering resource provider '$resourceProvider'"
+        Register-AzureRmResourceProvider -ProviderNamespace $resourceProvider
     }
 }
 
+#******************************************************************************
 #Create or check for existing resource group
+#******************************************************************************
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if(!$resourceGroup)
 {
-    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
-    if(!$resourceGroupLocation) {
-        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location."
+
+    if(!$resourceGroupLocation) 
+    {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation"
     }
-    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'"
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 }
-else{
-    Write-Host "Using existing resource group '$resourceGroupName'";
+else
+{
+    Write-Host "Using existing resource group '$resourceGroupName'"
+
 }
 
-# Start the deployment
-Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
-} else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+#******************************************************************************
+# Read Parameters File
+#******************************************************************************
+$ResolvedtemplateFilePath = Join-Path $PSScriptRoot $templateFilePath
+$ResolvedparametersFilePath = Join-Path $PSScriptRoot $parametersFilePath
+
+if(Test-Path $ResolvedparametersFilePath) {
+    $ParametersJson = Get-Content $ResolvedparametersFilePath
 }
+else
+{
+    $ParametersJson = @"
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+  "contentVersion": "1.0.0.0",
+  "parameters": {
+    "dbServerName": { "value": "" },
+    "dbDatabaseName": { "value": "" },
+    "DatabaseServerAdminLogin": { "value": "" },
+    "DatabaseServerAdminLoginPassword": {"value": ""}
+  }
+}
+"@
+}
+
+$parameterValues = $ParametersJson | ConvertFrom-Json
+    
+#******************************************************************************
+# Override Parms
+#******************************************************************************
+$ParameterOverrides = @{}
+
+$ParameterOverrides.dbServerName                     = $dbServerName
+$ParameterOverrides.dbDatabaseName                   = $dbDatabaseName
+$ParameterOverrides.DatabaseServerAdminLogin         = $DatabaseServerAdminLogin
+$ParameterOverrides.DatabaseServerAdminLoginPassword = $DatabaseServerAdminLoginPassword
+
+foreach($parameterOverrideName in $parameterOverrides.Keys)
+{
+    $parameterValues.parameters.$parameterOverrideName.value = $parameterOverrides.$parameterOverrideName
+}
+
+#******************************************************************************
+# Create Temp Parameters File
+#******************************************************************************
+$parameterTempFile =  New-TemporaryFile
+$parameterValues | ConvertTo-Json | Set-Content $parameterTempFile
+
+#******************************************************************************
+# Start the deployment
+#******************************************************************************
+Write-Host "Starting deployment..."
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $ResolvedtemplateFilePath -TemplateParameterFile $parameterTempFile #-Mode Complete -Force
